294c29d73ac51f0b98a10f033510cded0aab850d
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-24 10:37:59 +0900

    kamatakun version v0.1.7

--- kamatakun.rb
+++ kamatakun.rb
@@ -1,14 +1,15 @@
 class Kamatakun < Formula
   desc "kamata-kun!"
   homepage "https://github.com/zaru/kamatakun"
-  url "https://github.com/zaru/kamatakun/releases/download/v0.1.6/kamatakun_0.1.6_darwin_amd64.tar.gz"
-  version "0.1.6"
-  sha256 "011d8c3ed4745bd710f878f4de4e93dbdf5b56fe17b65824d5c452b17d1c03da"
+  url "https://github.com/zaru/kamatakun/releases/download/v0.1.7/kamatakun_0.1.7_darwin_amd64.tar.gz"
+  version "0.1.7"
+  sha256 "92dbe338545f8ff4e3ac6582370464615e57cdf0ad637ef90c54b4ff82f1cef6"
 
   depends_on "fzf"
 
   def install
     bin.install 'kamatakun'
+    bin.install 'kamatakun-cli'
   end
 
   test do
